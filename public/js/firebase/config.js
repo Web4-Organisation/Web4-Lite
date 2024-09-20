90dbd8ff889d44f63d8b43a75d54bdfcca28d792
Linkspreed <102324985+linkspreed@users.noreply.github.com>
2024-09-20 22:57:02 +0200

    Update config.js
    Signed-off-by: Linkspreed <102324985+linkspreed@users.noreply.github.com>

--- public/js/firebase/config.js
+++ public/js/firebase/config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDhrH8WLEB_H1BNl6jrgKdf4mvgdsff-Zw",
-    authDomain: "social-network-1077.firebaseapp.com",
-    databaseURL: "https://social-network-1077.firebaseio.com",
-    projectId: "social-network-1077",
-    storageBucket: "social-network-1077.appspot.com",
-    messagingSenderId: "729586005355",
-    appId: "1:729586005355:web:44cfb3f382887095fc082e"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
